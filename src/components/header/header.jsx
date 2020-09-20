158657f92337307ae2d7cc2ce52059fc783e15f4
AleksaKondricz <al.nikolovska@hotmail.com>
2020-09-20 13:29:49 +0200

    mobile header animated. firebase depolyed

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -3,6 +3,7 @@
 import React,{useState} from "react"
 import {navigate} from 'gatsby';
 import { Link, animateScroll as scroll } from "react-scroll"
+import Slide from "react-reveal/Slide"
 
 import linkProps from "./header.config"
 import navLinksData from "../../constants/navLinksData";
@@ -27,25 +28,21 @@ return(
       {navLinksData.map(l => <Link {...linkProps} to={l.id} className={styles.Link}>{l.label}</Link>)}
     </nav>
     }
-     {type==='formHeader' ? '' : <div className={styles.mobMenu} onClick={()=>toggleMobHeader(!mobHeader)}>{!mobHeader? <Menu/> : <span className={styles.closeMenu}>X</span>}</div>}
-     {mobHeader && 
+     {type==='formHeader' ? '' : <div className={styles.mobMenu} onClick={()=>toggleMobHeader(!mobHeader)}>{!mobHeader? <Menu/> : <span onClick={()=>toggleMobHeader(!mobHeader)} className={styles.closeMenu}>X</span>}</div>}
+  
+    {type==='formHeader' ? <span className={styles.formHeader} onClick={()=>navigate('/')}>X</span> : <Link to='cta' className={styles.estimate} {...linkProps}><span  /*onClick={()=>navigate('/getEstimation')}*/>Nice to have you here. &#128526;  </span></Link>}
+  </header>
+  {mobHeader && 
+     <Slide right duration={200}>
         <div className={styles.mobMenuExtended}>
-          <span onClick={()=>toggleMobHeader(!mobHeader)} className={styles.closeMenu}>X</span>
+   
           <nav>
           {navLinksData.map(l => <Link {...linkProps} offset={-200} onClick={() => toggleMobHeader(!mobHeader)} key={l.id} className={styles.Link} to={l.id}>{l.label}</Link>)}
           </nav>
           <img src={objects}/>
         </div>
+        </Slide>
      }
-    {type==='formHeader' ? <span className={styles.formHeader} onClick={()=>navigate('/')}>X</span> : <Link to='cta' className={styles.estimate} {...linkProps}><span  /*onClick={()=>navigate('/getEstimation')}*/>Nice to have you here. &#128526;  </span></Link>}
-  </header>
-  {
-    mobHeader && 
-    <header className={styles.headerMobile}>
-        <img src={logo}/>
-        <Menu/>
-    </header>
-  }
   </>
 )
 }
